fec6b0e12d8ebe025027423b4ae001a1d116ea52
intelheart <yanxin_951226@163.com>
2019-07-29 19:10:22 +0800

    上传

---
+++ VisualMVC/MVC/Base/NotDelete.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotDelete : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Object.DontDestroyOnLoad(this.gameObject);
+
+    }
+
+}
